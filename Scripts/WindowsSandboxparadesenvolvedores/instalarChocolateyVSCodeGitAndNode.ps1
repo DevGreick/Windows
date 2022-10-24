f1954998b2d058cfea80f500ca4d9a48b65de4b0
Jackson <jackson.greick@gmail.com>
2022-10-24 01:44:25 -0300

    Versao para desenvolvedores Versao para desenvolvedores

---
+++ Scripts/WindowsSandboxparadesenvolvedores/instalarChocolateyVSCodeGitAndNode.ps1
@@ -0,0 +1,12 @@
+<Configuration>
+<MappedFolders>
+<MappedFolder>
+<HostFolder>C:\Gutts\Sandbox</HostFolder>
+<SandboxFolder>C:\Users\WDAGUtilityAccount\Downloads</SandboxFolder>
+<ReadOnly>true</ReadOnly>
+</MappedFolder>
+</MappedFolders>
+<LogonCommand>
+<Command>powershell -executionpolicy unrestricted -command "start powershell {-noexit -file C:\Users\WDAGUtilityAccount\Downloads\Arquivos\InstalarChocolateyVSCodeGitAndNode.ps1}"</Command>
+</LogonCommand>
+</Configuration>

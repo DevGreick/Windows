472f188d8f8ad52170524100aebd49bdef007f1a
Jackson <jackson.greick@gmail.com>
2022-10-25 23:42:16 -0300

    Alteração script

--- Scripts/Criar Pasta oculta com senha/locker.bat
+++ Scripts/Criar Pasta oculta com senha/locker.bat
@@ -2,7 +2,7 @@ cls
 @ECHO OFF
 title Pasta Oculta 
 if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
-if NOT EXIST Private goto MDLOCKER
+if NOT EXIST Privado goto MDLOCKER
 :CONFIRM
 echo Tem certeza de que deseja bloquear a pasta (Y/N)
 set/p "cho=>"
@@ -10,26 +10,27 @@ if %cho%==Y goto LOCK
 if %cho%==y goto LOCK
 if %cho%==n goto END
 if %cho%==N goto END
-echo Invalid choice.
+echo Escolha invalida.
 goto CONFIRM
 :LOCK
-ren Private "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ren Privado "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
-echo Folder locked
+echo Pasta bloqueada
 goto End
 :UNLOCK
 echo Digite a senha para desbloquear pasta
 set/p "pass=>"
-if NOT %pass%== suasenhaaqui goto FAIL
+if NOT %pass%== digiteasenhaaqui goto FAIL
 attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
-ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Private
+ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Privado
 echo Folder Desbloqueado com sucesso
 goto End
 :FAIL
 echo Senha Invalida
 goto end
 :MDLOCKER
-md Private
-echo Private foi criada com sucesso
+md Privado
+echo Privado foi criada com sucesso
 goto End
 :End
+pause
